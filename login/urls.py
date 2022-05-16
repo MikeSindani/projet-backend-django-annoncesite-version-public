ffe976592b79f14538b358fe002047928a213f68
MikeSindani <mikems@live.fr>
2022-05-16 12:57:53 +0200

    mise a jour

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('signIn/', views.signIn ,name='signIn'),
-   path('signUp/', views.signUp ,name='signUp'),
+   path('signIn/', views.signIn ,  name='signIn'),
+   path('signUp/', views.signUp , name='signUp'),
 ]
