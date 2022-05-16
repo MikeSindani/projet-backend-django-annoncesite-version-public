66829537386fad0d10da2027c81f5da027756909
MikeSindani <mikems@live.fr>
2022-05-16 13:49:51 +0200

    mis a jour leger mai 2022

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def signIn(request):
-    return render(request,"signIn/signIn.html")
+    return render(request,"login/signIn.html")
 def signUp(request):
-    return render(request,"signUp/signUp.html")
+    return render(request,"login/signUp.html")
     
