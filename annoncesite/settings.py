a83b922738557bb9f3e5917bf8f1b27b28e1c37f
MikeSindani <mikems@live.fr>
2022-06-08 18:20:46 +0200

    version 1.0282

--- annoncesite/settings.py
+++ annoncesite/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
     ]
 
-
+WHITENOISE_USE_FINDERS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
