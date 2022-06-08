abed7b323752d1fea964929c5b28a27d111c7efd
MikeSindani <mikems@live.fr>
2022-06-08 17:58:01 +0200

    version 1.027

--- annoncesite/settings.py
+++ annoncesite/settings.py
@@ -122,15 +122,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
     ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'       
+      
