b00cff78e83269a7b6cfe0a75c70d1b467d8ea9f
MikeSindani <mikems@live.fr>
2022-06-09 19:04:24 +0200

    ameliorer section elevage et agricole

--- static/firebase/firebase_site.js
+++ static/firebase/firebase_site.js
@@ -1,19 +1,20 @@
-    // Your web app's Firebase configuration
-      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-      const Config = {
-        apiKey: "AIzaSyBuwWCDpsLuwcrygf5qGuTkiznBMjFclNA",
-        authDomain: "annoncesite-5d608.firebaseapp.com",
-        databaseURL: "https://annoncesite-5d608-default-rtdb.firebaseio.com",
-        projectId: "annoncesite-5d608",
-        storageBucket: "annoncesite-5d608.appspot.com",
-        messagingSenderId: "729688890729",
-        appId: "1:729688890729:web:30db15c91ef10f9d2e31f8",
-        measurementId: "G-E13XZ4TGFL"
-      };
-      firebase.initializeApp(Config);
+      
       // Get a reference to the storage service, which is used to create references in your storage bucket
-
+         
           function uploadimage(){
+             // Your web app's Firebase configuration
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+            const Config = {
+              apiKey: "AIzaSyBuwWCDpsLuwcrygf5qGuTkiznBMjFclNA",
+              authDomain: "annoncesite-5d608.firebaseapp.com",
+              databaseURL: "https://annoncesite-5d608-default-rtdb.firebaseio.com",
+              projectId: "annoncesite-5d608",
+              storageBucket: "annoncesite-5d608.appspot.com",
+              messagingSenderId: "729688890729",
+              appId: "1:729688890729:web:30db15c91ef10f9d2e31f8",
+              measurementId: "G-E13XZ4TGFL"
+            };
+            firebase.initializeApp(Config);
 
               var storage = firebase.storage();
 
@@ -39,6 +40,6 @@
           document.getElementById("url").value = downloadURL;
           console.log("file uploaded successfully");
           var btnLogin = document.getElementById("loginBtn").disabled = false;
-
+          document.getElementById("message").innerHTML=""     
         });
       }
