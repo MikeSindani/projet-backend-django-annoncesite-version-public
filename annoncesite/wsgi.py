e5050c4fa3ebbfc9e86606bfa295b8c0cf151f0f
MikeSindani <mikems@live.fr>
2023-02-05 02:17:44 +0200

    for vercel deploiment

--- annoncesite/wsgi.py
+++ annoncesite/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'annoncesite.settings')
 
 application = get_wsgi_application()
+
+
+# ajouter pour vercel
+app = application
