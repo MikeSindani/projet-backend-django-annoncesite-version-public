c8d40671f08678932ff51ccfdf1094e1bf663446
MikeSindani <mikems@live.fr>
2023-02-05 22:59:16 +0200

    vercel test 5

--- annoncesite/urls.py
+++ annoncesite/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from django.conf.urls import handler404, handler500
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,6 +20,10 @@ urlpatterns = [
     path('',include('mobile_app.urls')), 
 ]
 
-
+# pour les page 404 et 500 
 handler404 = 'error_page_app.views.error_404'
 handler500 = 'error_page_app.views.error_500'
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
