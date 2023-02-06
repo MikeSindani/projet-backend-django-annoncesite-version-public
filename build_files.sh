c0c52249dfe5ef03ae3eb4a08ba848f03c145cd6
MikeSindani <mikems@live.fr>
2023-02-06 15:09:25 +0200

    vercel 10

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
 echo " BUILD START"
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
-python3.9 manage.py runserver
+python3.10 manage.py collectstatic
+python3.10 manage.py runserver
 echo " BUILD END"
 echo " DEBUT VERCEL STATIC MIKE SINDANI "
