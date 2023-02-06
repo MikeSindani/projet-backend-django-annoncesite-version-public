ebef1dbbe81b5e0190222756588858018365c45a
Mike Sindani <82335605+MikeSindani@users.noreply.github.com>
2023-02-06 14:34:43 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 # build_files.sh
 echo " BUILD START"
+python3.9 -m venv venv
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python3.9 manage.py runserver
 echo " BUILD END"
 echo " DEBUT VERCEL STATIC "
