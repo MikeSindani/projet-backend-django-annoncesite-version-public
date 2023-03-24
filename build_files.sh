1553c7d5bfe863708c0f9716bae2eb4470de275e
MikeSindani <mikems@live.fr>
2023-03-24 21:44:07 +0200

    annonce test vercel 2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 # build_files.sh
 echo " BUILD START"
+python -m venv annonce-env
+source annonce-env/bin/activate
 pip install -r requirements.txt
 python3.10 manage.py collectstatic
 python3.10 manage.py runserver
