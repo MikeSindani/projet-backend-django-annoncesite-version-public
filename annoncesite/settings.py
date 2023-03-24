75ee2ccb33c8e2ecc4aae69976ba26f1f9a37ee8
MikeSindani <mikems@live.fr>
2023-03-24 21:32:08 +0200

    test with vercel 24/03/2022

--- annoncesite/settings.py
+++ annoncesite/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'annoncesite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
