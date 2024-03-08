11377f3513568cef4de3f9bdfad0a586e430ead5
Mike Sindani <82335605+MikeSindani@users.noreply.github.com>
2024-03-09 00:35:31 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ echo " BUILD START"
 python -m venv annonce-env
 source annonce-env/bin/activate
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
-python3.10 manage.py runserver
+python manage.py collectstatic
+python manage.py runserver
 echo " BUILD END"
 echo " DEBUT VERCEL STATIC MIKE SINDANI "
